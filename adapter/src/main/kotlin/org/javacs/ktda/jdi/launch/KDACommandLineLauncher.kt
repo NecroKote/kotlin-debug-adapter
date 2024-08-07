468b192f007fe5f963e1ece579babcc6bf9d0282
Mikalai Kukhta <mikalai.kukhta@epidemicsound.com>
2024-08-07 13:26:05 +0200

    prettify KDACommandLineLauncher code a little. remove external dependency on org.codehaus.plexus.util.cli.CommandLineUtils

--- adapter/src/main/kotlin/org/javacs/ktda/jdi/launch/KDACommandLineLauncher.kt
+++ adapter/src/main/kotlin/org/javacs/ktda/jdi/launch/KDACommandLineLauncher.kt
@@ -5,10 +5,9 @@ import com.sun.jdi.VirtualMachine
 import com.sun.jdi.connect.*
 import com.sun.jdi.connect.spi.Connection
 import com.sun.jdi.connect.spi.TransportService
-import org.codehaus.plexus.util.cli.CommandLineUtils
-import org.javacs.kt.LOG
 import java.io.File
 import java.io.IOException
+import java.io.InputStream
 import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -16,8 +15,10 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
 import java.util.concurrent.Callable
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import org.javacs.kt.LOG
 
 internal const val ARG_HOME = "home"
 internal const val ARG_OPTIONS = "options"
@@ -28,18 +29,14 @@ internal const val ARG_VM_EXEC = "vmexec"
 internal const val ARG_CWD = "cwd"
 internal const val ARG_ENV = "env"
 
-/**
- * A custom LaunchingConnector that supports cwd and env variables
- */
+/** A custom LaunchingConnector that supports cwd and env variables */
 open class KDACommandLineLauncher : LaunchingConnector {
 
     protected val defaultArguments = mutableMapOf<String, Connector.Argument>()
 
-    /**
-     * We only support SocketTransportService
-     */
-    protected var transportService : TransportService? = null
-    protected var transport = Transport { "dt_socket" }
+    /** We only support SocketTransportService */
+    protected val transportService: TransportService
+    protected val transport = Transport { "dt_socket" }
 
     companion object {
 
@@ -55,226 +52,231 @@ open class KDACommandLineLauncher : LaunchingConnector {
     }
 
     constructor() : super() {
-
-        defaultArguments[ARG_HOME] = StringArgument(ARG_HOME, description = "Java home", value = System.getProperty("java.home"))
-
-        defaultArguments[ARG_OPTIONS] = StringArgument(ARG_OPTIONS, description = "Jvm arguments")
-
-        defaultArguments[ARG_MAIN] = StringArgument(ARG_MAIN, description = "Main class name and parameters", mustSpecify = true)
-
-        defaultArguments[ARG_SUSPEND] = StringArgument(ARG_SUSPEND, description = "Whether launch the debugee in suspend mode", value = "true")
-
-        defaultArguments[ARG_QUOTE] = StringArgument(ARG_QUOTE, description = "Quote char", value = "\"")
-
-        defaultArguments[ARG_VM_EXEC] = StringArgument(ARG_VM_EXEC, description = "The java exec", value = "java")
-
-        defaultArguments[ARG_CWD] = StringArgument(ARG_CWD, description = "Current working directory")
-
-        defaultArguments[ARG_ENVS] = StringArgument(ARG_ENVS, description = "Environment variables")
-
-        // Load TransportService 's implementation
-        transportService = Class.forName("com.sun.tools.jdi.SocketTransportService").getDeclaredConstructor().newInstance() as TransportService
-
-        if(transportService == null){
-            throw IllegalStateException("Failed to load com.sun.tools.jdi.SocketTransportService")
+        defaultArguments.apply {
+            set(ARG_HOME, StringArgument(ARG_HOME, "Java home", value = System.getProperty("java.home")))
+            set(ARG_OPTIONS, StringArgument(ARG_OPTIONS, "Jvm arguments"))
+            set(ARG_MAIN, StringArgument(ARG_MAIN, "Main class name and parameters", mustSpecify = true))
+            set(ARG_SUSPEND, StringArgument(ARG_SUSPEND, "Whether launch the debugee in suspend mode", "true"))
+            set(ARG_QUOTE, StringArgument(ARG_QUOTE, "Quote char", value = "\""))
+            set(ARG_VM_EXEC, StringArgument(ARG_VM_EXEC, "The java exec", value = "java"))
+            set(ARG_CWD, StringArgument(ARG_CWD, "Current working directory"))
+            set(ARG_ENV, StringArgument(ARG_ENV, "Environment variables"))
         }
 
+        // Load TransportService 's implementation
+        try {
+            transportService =
+                    Class.forName("com.sun.tools.jdi.SocketTransportService")
+                            .getDeclaredConstructor()
+                            .newInstance() as
+                            TransportService
+        } catch (e: Exception) {
+            throw IllegalStateException("Failed to load com.sun.tools.jdi.SocketTransportService")
+        }
     }
 
-    override fun name(): String {
-        return this.javaClass.name
-    }
+    override fun name(): String = javaClass.name
 
-    override fun description(): String {
-        return "A custom launcher supporting cwd and env variables"
-    }
+    override fun description(): String = "A custom launcher supporting cwd and env variables"
 
-    override fun defaultArguments(): Map<String, Connector.Argument> {
-        return this.defaultArguments
-    }
+    override fun defaultArguments(): Map<String, Connector.Argument> = defaultArguments
 
-    override fun toString(): String {
-        return name()
-    }
+    override fun toString(): String = name()
 
-    override fun transport(): Transport {
-        return transport
-    }
+    override fun transport(): Transport = transport
 
-    protected fun getOrDefault(arguments: Map<String, Connector.Argument>, argName: String): String {
+    protected fun getOrDefault(
+        arguments: Map<String, Connector.Argument>,
+        argName: String
+    ): String {
         return arguments[argName]?.value() ?: defaultArguments[argName]?.value() ?: ""
     }
 
-    protected fun translateCommandline(toProcess: String): Array<String> {
-        if (toProcess.length == 0 ) return emptyArray()
-
-        // parse with a simple finite state machine
-        val normal = 0;
-        val inQuote = 1;
-        val inDoubleQuote = 2;
-        var state = normal;
-
-        val tok = StringTokenizer( toProcess, "\"\' ", true );
-        var v = mutableListOf<String>();
-        val current = StringBuilder();
-
-        while ( tok.hasMoreTokens() ) {
-            val nextTok = tok.nextToken();
-            when ( state ) {
-                inQuote -> {
-                    if ( "\'".equals( nextTok ) )
-                    {
-                        state = normal;
-                    }
-                    else
-                    {
-                        current.append( nextTok );
-                    }
-                }
-                inDoubleQuote -> {
-                    if ( "\"".equals( nextTok ) )
-                    {
-                        state = normal;
-                    }
-                    else
-                    {
-                        current.append( nextTok );
-                    }
-                }
-                else -> {
-                    if ( "\'".equals( nextTok ) )
-                    {
-                        state = inQuote;
-                    }
-                    else if ( "\"".equals( nextTok ) )
-                    {
-                        state = inDoubleQuote;
-                    }
-                    else if ( " ".equals( nextTok ) )
-                    {
-                        if ( current.length != 0 )
-                        {
-                            v.add( current.toString() );
-                            current.setLength( 0 );
-                        }
-                    }
-                    else
-                    {
-                        current.append( nextTok );
-                    }
-                    break;
+    private fun tokenizeCommandLine(args: String): Array<String> {
+        val result = mutableListOf<String>()
+    
+        val DEFAULT = 0
+        val ARG = 1
+        val IN_DOUBLE_QUOTE = 2
+        val IN_SINGLE_QUOTE = 3
+    
+        var state = DEFAULT
+        val buf = StringBuilder()
+        val len = args.length
+        var i = 0
+    
+        while (i < len) {
+            var ch = args[i]
+            if (ch.isWhitespace()) {
+                if (state == DEFAULT) {
+                    // skip
+                    i++
+                    continue
+                } else if (state == ARG) {
+                    state = DEFAULT
+                    result.add(buf.toString())
+                    buf.setLength(0)
+                    i++
+                    continue
                 }
             }
+            when (state) {
+                DEFAULT, ARG -> {
+                    when {
+                        ch == '"' -> state = IN_DOUBLE_QUOTE
+                        ch == '\'' -> state = IN_SINGLE_QUOTE
+                        ch == '\\' && i + 1 < len -> {
+                            state = ARG
+                            ch = args[++i]
+                            buf.append(ch)
+                        }
+                        else -> {
+                            state = ARG
+                            buf.append(ch)
+                        }
+                    }
+                }
+                IN_DOUBLE_QUOTE -> {
+                    when {
+                        ch == '"' -> state = ARG
+                        ch == '\\' && i + 1 < len && (args[i + 1] == '\\' || args[i + 1] == '"') -> {
+                            ch = args[++i]
+                            buf.append(ch)
+                        }
+                        else -> buf.append(ch)
+                    }
+                }
+                IN_SINGLE_QUOTE -> {
+                    if (ch == '\'') {
+                        state = ARG
+                    } else {
+                        buf.append(ch)
+                    }
+                }
+                else -> throw IllegalStateException()
+            }
+            i++
         }
-
-        if (current.length != 0) v.add( current.toString() )
-
-        if ( ( state == inQuote ) || ( state == inDoubleQuote ) )
-        {
-            throw Exception( "unbalanced quotes in " + toProcess );
+        if (buf.isNotEmpty() || state != DEFAULT) {
+            result.add(buf.toString())
         }
-
-        return v.toTypedArray()
+    
+        return result.toTypedArray()
     }
 
-    /**
-     * A customized method to launch the vm and connect to it, supporting cwd and env variables
-     */
-    @Throws(IOException::class, IllegalConnectorArgumentsException::class, VMStartException::class)
-    override fun launch(arguments: Map<String, Connector.Argument>): VirtualMachine {
-        val vm: VirtualMachine
-
+    private fun buildCommandLine(arguments: Map<String, Connector.Argument>, address: String): String {
         val home = getOrDefault(arguments, ARG_HOME)
+        val javaExe = getOrDefault(arguments, ARG_VM_EXEC)
         val options = getOrDefault(arguments, ARG_OPTIONS)
-        val main = getOrDefault(arguments, ARG_MAIN)
         val suspend = getOrDefault(arguments, ARG_SUSPEND).toBoolean()
-        val quote = getOrDefault(arguments, ARG_QUOTE)
-        var exe = getOrDefault(arguments, ARG_VM_EXEC)
-        val cwd = getOrDefault(arguments, ARG_CWD)
-        val envs = urlDecode(getOrDefault(arguments, ARG_ENVS))?.toTypedArray()
+        val main = getOrDefault(arguments, ARG_MAIN)
+        
+        val exe = if (home.isNotEmpty()) Paths.get(home, "bin", javaExe).toString() else javaExe
 
-        check(quote.length == 1) {"Invalid length for $ARG_QUOTE: $quote"}
-        check(!options.contains("-Djava.compiler=") ||
-                options.toLowerCase().contains("-djava.compiler=none")) { "Cannot debug with a JIT compiler. $ARG_OPTIONS: $options"}
-
-        val listenKey = transportService?.startListening() ?: throw IllegalStateException("Failed to do transportService.startListening()")
-        val address = listenKey.address()
-
-        try {
-            val command = StringBuilder()
-
-            exe = if (home.isNotEmpty()) Paths.get(home, "bin", exe).toString() else exe
-            command.append(wrapWhitespace(exe))
-
-            command.append(" $options")
-
-            //debug options
-            command.append(" -agentlib:jdwp=transport=${transport.name()},address=$address,server=n,suspend=${if (suspend) 'y' else 'n'}")
-
-            command.append(" $main")
-
-            LOG.debug("command before tokenize: $command")
-
-            vm = launch(commandArray = this.translateCommandline(command.toString()), listenKey = listenKey,
-                    ts = transportService!!, cwd = cwd, envs = envs
+        return StringBuilder().apply {
+            append("$exe")
+            val jdwpLine = arrayOf(
+                "transport=${transport.name()}",
+                "address=$address",
+                "server=n",
+                "suspend=${if (suspend) 'y' else 'n'}"
             )
+            append(" -agentlib:jdwp=${jdwpLine.joinToString(",")}")
+            append(" $options")
+            append(" $main")
+        }.toString()
+    }
 
-        } finally {
-            transportService?.stopListening(listenKey)
+    /** A customized method to launch the vm and connect to it, supporting cwd and env variables */
+    @Throws(IOException::class, IllegalConnectorArgumentsException::class, VMStartException::class)
+    override fun launch(arguments: Map<String, Connector.Argument>): VirtualMachine {        
+        val quote = getOrDefault(arguments, ARG_QUOTE)
+        val options = getOrDefault(arguments, ARG_OPTIONS)
+        val cwd = getOrDefault(arguments, ARG_CWD)
+        val env = urlDecode(getOrDefault(arguments, ARG_ENV))?.toTypedArray()
+
+        check(quote.length == 1) { "Invalid length for $ARG_QUOTE: $quote" }
+        check(!options.contains("-Djava.compiler=") || options.lowercase().contains("-djava.compiler=none")) {
+            "Cannot debug with a JIT compiler. $ARG_OPTIONS: $options" 
         }
-        return vm
+
+        val listenKey = transportService.startListening()
+        ?: throw IllegalStateException("Failed to do transportService.startListening()")
+
+        val command = buildCommandLine(arguments, listenKey.address())
+        val tokenizedCommand = tokenizeCommandLine(command)
+
+        val (connection, process) = launchAndConnect(tokenizedCommand, listenKey, transportService, cwd, env)
+        return Bootstrap.virtualMachineManager().createVirtualMachine(connection, process)
     }
 
-    internal fun wrapWhitespace(str: String): String {
-       return if(str.contains(' ')) "\"$str\" " else str
+    private fun streamToString(stream: InputStream): String {
+        val lineSep = System.getProperty("line.separator")
+        return StringBuilder().apply {
+            var line: String?
+            val reader = stream.bufferedReader()
+
+            while (reader.readLine().also { line = it } != null) {
+                append(line)
+                append(lineSep)
+            }
+        }.toString()
     }
-
-    @Throws(IOException::class, VMStartException::class)
-    fun launch(commandArray: Array<String>,
-                        listenKey: TransportService.ListenKey,
-                        ts: TransportService, cwd: String, envs: Array<String>? = null): VirtualMachine {
-
-        val (connection, process) = launchAndConnect(commandArray, listenKey, ts, cwd = cwd, envs = envs)
-
-        return Bootstrap.virtualMachineManager().createVirtualMachine(connection,
-                process)
-    }
-
 
     /**
      * launch the command, connect to transportService, and returns the connection / process pair
      */
-    protected fun launchAndConnect(commandArray: Array<String>, listenKey: TransportService.ListenKey,
-                         ts: TransportService, cwd: String = "", envs: Array<String>? = null): Pair<Connection, Process>{
+    protected fun launchAndConnect(
+            commandArray: Array<String>,
+            listenKey: TransportService.ListenKey,
+            ts: TransportService,
+            cwd: String = "",
+            envs: Array<String>? = null
+    ): Pair<Connection, Process> {
 
-        val dir = if(cwd.isNotBlank() && Files.isDirectory(Paths.get(cwd))) File(cwd) else null
+        val dir = if (cwd.isNotBlank() && Files.isDirectory(Paths.get(cwd))) File(cwd) else null
 
-        var threadCount = 0
-
-        val executors = Executors.newFixedThreadPool(2) { Thread(it, "${this.javaClass.simpleName}-${threadCount++}") }
         val process = Runtime.getRuntime().exec(commandArray, envs, dir)
 
-        val connectionTask: Callable<Any> = Callable { ts.accept(listenKey, 0,0).also { LOG.debug("ts.accept invoked") } }
-        val exitCodeTask: Callable<Any> = Callable { process.waitFor().also { LOG.debug("process.waitFor invoked") } }
+        val result = CompletableFuture<Pair<Connection, Process>>()
 
-        try {
-            when (val result = executors.invokeAny(listOf(connectionTask, exitCodeTask))) {
-                // successfully connected to transport service
-                is Connection -> return Pair(result, process)
+        process.onExit().thenAcceptAsync() { pr ->
+            if (result.isDone) return@thenAcceptAsync
 
-                // cmd exited before connection. some thing wrong
-                is Int -> throw VMStartException(
-                        "VM initialization failed. exit code: ${process?.exitValue()}, cmd: $commandArray", process)
+            val exitCode = pr.exitValue()
+            val stdOut = streamToString(process.inputStream)
+            val stdErr = streamToString(process.errorStream)
 
-                // should never occur
-                else -> throw IllegalStateException("Unknown result: $result")
-            }
-        } finally {
-            // release the executors. no longer needed.
-            executors.shutdown()
-            executors.awaitTermination(1, TimeUnit.SECONDS)
+            LOG.error("Process exited with status: ${exitCode} \n $stdOut \n $stdErr")
+            result.completeExceptionally(VMStartException("Process exited with status: ${exitCode}", process))
+
+            try {
+                ts.stopListening(listenKey)
+            } catch (e: Exception) {}
         }
 
-    }
+        CompletableFuture.runAsync() {
+            try {
+                val vm = ts.accept(listenKey, 5_000, 5_000)
+                result.complete(Pair(vm, process))
+            } catch (e: IllegalConnectorArgumentsException) {
+                result.completeExceptionally(e)
+            } catch (e: IOException) {
+                if (result.isDone) return@runAsync
 
+                val stdOut = streamToString(process.inputStream)
+                val stdErr = streamToString(process.errorStream)
+
+                LOG.error("Failed to connect to the launched process: \n $stdOut \n $stdErr")
+
+                process.destroy()
+
+                result.completeExceptionally(e)
+            } catch (e: RuntimeException) {
+                result.completeExceptionally(e)
+            }
+        }
+
+        return result.get()
+    }
 }
